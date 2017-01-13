dceb93096c2f6f605dc8508cc2dd86de34199a9a
Rohan MALLELWAR (contractor) <rohan.mallelwar@amadeus.com>
2017-01-13 22:21:18 +0530

    Markers Added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MapIt!';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+ // title = 'MapIt!';
+  zoom: number = 12;
+  lat: number =  12.913425;
+  lng: number = 77.596502;
+
+  markers : marker[] = [
+    {
+      name : "Company One",
+      lat : 12.913624,
+      lng: 77.593337,
+      draggable : true
+    },
+    {
+      name : "Company Two",
+      lat : 12.933624,
+      lng: 77.513337,
+      draggable : true
+    },
+    {
+      name : "Company One3",
+      lat : 12.913425,
+      lng: 77.596502,
+      draggable : true
+    },
+  ];
+  constructor(){
+
+  }
+
+}
+interface marker {
+  name?:string;
+  lat: number;
+  lng: number;
+  draggable: boolean;
 }
