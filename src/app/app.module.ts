56dabbc7ae882f0d13acc4396315af32f579d5d7
Rohan MALLELWAR (contractor) <rohan.mallelwar@amadeus.com>
2017-01-13 17:10:58 +0530

    Adding Google Map API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from 'angular2-google-maps/core';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDiRftW75iuFtaDca2bIHR-owEGo9YDWCg'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
